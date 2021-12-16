4ced55440d28e9a90d25dbfe3eaa5219c2ccc27c
Your Github User Name <ramirezr@protonmail.com>
2021-12-16 10:14:41 -0600

    farmer login working

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,98 +1,3 @@
-// import React, { useState } from "react"
-// import { useHistory } from "react-router-dom";
-// // import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
-// import "./Login.css"
-
-// export const Register = () => {
-//     const [credentials, syncAuth] = useState({
-//         firstName: "",
-//         lastName: "",
-//         email: "",
-//         farmer: false
-//     })
-//     // const { register } = useSimpleAuth()
-//     const register = useHistory()
-//     const history = useHistory()
-
-//     const handleRegister = (e) => {
-//         e.preventDefault()
-
-//         const newUser = {
-//             name: `${credentials.firstName} ${credentials.lastName}`,
-//             email: credentials.email,
-//             farmer: credentials.farmer
-//         }
-
-//         register(newUser).then(() => {
-//             history.push("/")
-//         })
-//     }
-
-//     const handleUserInput = (event) => {
-//         const copy = {...credentials}
-//         copy[event.target.id] = event.target.value
-//         syncAuth(copy)
-//     }
-
-
-//     return (
-//         <main style={{ textAlign: "center" }}>
-//             <form className="form--login" onSubmit={handleRegister}>
-//                 <h1 className="h3 mb-3 font-weight-normal">Please Register for FarmAlicious</h1>
-//                 <fieldset>
-//                     <label htmlFor="firstName"> First Name </label>
-//                     <input type="text" onChange={handleUserInput}
-//                         id="firstName"
-//                         className="form-control"
-//                         placeholder="First name"
-//                         required autoFocus />
-//                 </fieldset>
-//                 <fieldset>
-//                     <label htmlFor="lastName"> Last Name </label>
-//                     <input type="text" onChange={handleUserInput}
-//                         id="lastName"
-//                         className="form-control"
-//                         placeholder="Last name"
-//                         required />
-//                 </fieldset>
-//                 <fieldset>
-//                     <label htmlFor="inputEmail"> Email address </label>
-//                     <input type="email" onChange={handleUserInput}
-//                         id="email"
-//                         className="form-control"
-//                         placeholder="Email address"
-//                         required />
-//                 </fieldset>
-//                 <fieldset>
-//                     <input
-//                         onChange={
-//                             (event) => {
-//                                 const copy = { ...credentials }
-//                                 if (event.target.value === "on") {
-//                                     copy.farmer = true
-//                                 }
-//                                 else {
-//                                     copy.farmer = false
-//                                 }
-//                                 syncAuth(copy)
-//                             }
-//                         }
-//                         defaultChecked={credentials.farmer}
-//                         type="checkbox" name="farmer" id="farmer" />
-//                     <label htmlFor="farmer"> Farmer account? </label>
-//                 </fieldset>
-
-//                 <fieldset>
-//                     <button type="submit">
-//                         Sign in
-//                     </button>
-//                 </fieldset>
-//             </form>
-//         </main>
-//     )
-// }
-
-
 import React, { useRef, useState } from "react"
 import { useHistory } from "react-router-dom"
 import "./Login.css"
@@ -104,9 +9,8 @@ export const Register = (props) => {
         firstName: "",
         lastName: "",
         email: "",
-        farmer: false
+        farmer: ""
     })
-
 
     const history = useHistory()
 
@@ -125,7 +29,7 @@ export const Register = (props) => {
                         headers: {
                             "Content-Type": "application/json"
                         },
-                        body: JSON.stringify(user)
+                        body: JSON.stringify(credentials)
                     })
                         .then(res => res.json())
                         .then(createdUser => {
@@ -142,9 +46,9 @@ export const Register = (props) => {
     }
 
     const updateUser = (evt) => {
-        const copy = {...user}
+        const copy = {...credentials}
         copy[evt.target.id] = evt.target.value
-        setUser(copy)
+        syncAuth(copy)
     }
 
 
